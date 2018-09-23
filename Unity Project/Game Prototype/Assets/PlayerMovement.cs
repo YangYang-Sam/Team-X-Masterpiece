5ce69ec6476492bf71a1dbcf31956a7b995199ce
Jonny-S <43474013+Jonny-S@users.noreply.github.com>
2018-09-23 21:57:15 +0100

    Implemented movement prototype
    Implemented sample scene and character with sample assets from asset store. Implemented movement template for display and prototype purposes.

---
+++ Unity Project/Game Prototype/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    public float runspeed = 40f;
+
+    float horizontalMovement = 0f;
+    bool jump = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runspeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
---
+++ Unity Project/Game Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpforce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+}
