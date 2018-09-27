b075eadb41fe7c5803b3714963bc587268ba6d67
HappinessDrone <43468076+HappinessDrone@users.noreply.github.com>
2018-09-27 16:03:56 +0100

    Revert "Merge branch 'master' of https://github.com/YangYang-Sam/Team-X-Masterpiece"
    This reverts commit ad92c959f12e4c76b62447a8419c53b52fc2ff16, reversing
    changes made to da5f0a10b942d0a19b6c30e6af9cbd2d5e6dc672.

---
+++ Unity Project/Game Prototype/Assets/PlayerMovement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    public float runspeed = 40f;
+
+    float horizontalMovement = 0f;
+    bool jump = false;
+
+    //[SerializeField]
+    private bool plane1Invisible = false;
+    //[SerializeField]
+    private bool plane2Invisible = true;
+    //[SerializeField]
+    private bool plane3Invisible = true;
+
+    private GameObject portal1;
+    private GameObject portal2;
+    private GameObject portal3;
+
+    public const string Plane1Layer = "Foreground";
+    public const string Plane2Layer = "Middleground";
+    public const string Plane3Layer = "Background";
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite)
+        {
+            sprite.sortingOrder = sortingOrder;
+            sprite.sortingLayerName = Plane1Layer;
+        }
+
+        Debug.Log(LayerMask.NameToLayer("Plane 1"));
+        Debug.Log(LayerMask.NameToLayer("Plane 2"));
+        Debug.Log(LayerMask.NameToLayer("Plane 3"));
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runspeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //Ignore the collisions between layer 8 (player) and platform layers
+        Physics2D.IgnoreLayerCollision(8, 10, plane1Invisible);
+        Physics2D.IgnoreLayerCollision(8, 11, plane2Invisible);
+        Physics2D.IgnoreLayerCollision(8, 12, plane3Invisible);
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collided with: " + other.name);
+        portal1 = GameObject.Find("Portal 1");
+        portal2 = GameObject.Find("Portal 2");
+        portal3 = GameObject.Find("Portal 3");
+
+        if (other.name == "Portal 1")
+        {
+
+                plane1Invisible = true;
+                plane2Invisible = false;
+                plane3Invisible = true;
+
+            //GameObject.Find("Plane 1 Platforms").transform.localScale = new Vector3(0, 0, 0);
+            Destroy(portal1.gameObject);
+            sprite.sortingLayerName = Plane2Layer;
+
+        }
+
+        else if (other.name == "Portal 2")
+            {
+
+                plane1Invisible = true;
+                plane2Invisible = true;
+                plane3Invisible = false;
+
+            //GameObject.Find("Plane 2 Platforms").transform.localScale = new Vector3(0, 0, 0);
+            Destroy(portal2.gameObject);
+            sprite.sortingLayerName = Plane3Layer;
+        }
+
+        else if (other.name == "Portal 3")
+        {
+
+                plane1Invisible = false;
+                plane2Invisible = true;
+                plane3Invisible = true;
+
+            //GameObject.Find("Plane 3 Platforms").transform.localScale = new Vector3(0, 0, 0);
+            Destroy(portal3.gameObject);
+            sprite.sortingLayerName = Plane1Layer;
+
+        }
+
+    }
+
+}
---
+++ Unity Project/Game Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpforce;
+    private float moveInput;
+
+    private Rigidbody2D m_Rigidbody;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    private void Start()
+    {
+
+        //Fetch the Rigidbody component from the GameObject
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        //Ignore the collisions between layer 8 (player) and layer 8 (custom layer you set in Inspector window)
+        Physics2D.IgnoreLayerCollision(8, 10);
+    }
+
+    private void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        m_Rigidbody.velocity = new Vector2(moveInput * speed, m_Rigidbody.velocity.y);
+
+    }
+
+}
