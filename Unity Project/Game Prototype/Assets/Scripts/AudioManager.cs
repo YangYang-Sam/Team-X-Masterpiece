c7f7d3d748c1eee3cafa4654c82439d01bae4d62
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-10-02 17:14:24 +0100

    Small tweaks to audio and plane systems

--- Unity Project/Game Prototype/Assets/Scripts/AudioManager.cs
+++ Unity Project/Game Prototype/Assets/Scripts/AudioManager.cs
@@ -6,8 +6,21 @@ public class AudioManager : MonoBehaviour {
 
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
 	// Use this for initialization
 	void Awake () {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -25,6 +38,11 @@ public class AudioManager : MonoBehaviour {
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
         s.source.Play();
     }
 
