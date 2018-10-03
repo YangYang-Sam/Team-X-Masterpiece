927fb10d7739841a0555ea08865ed89efdfa3a21
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-10-03 14:43:54 +0100

    Implemented Animated Portals and New Tilesets

---
+++ Programming/Game Prototype/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    //set variables to store speed etc and alter fields in inspector 
+    private float _speed;
+    [SerializeField]
+    private float _speedValue;
+    [SerializeField]
+    private float jumpforce;
+    [SerializeField]
+    private float _veilJumpForce;
+    private float horizontalMove;
+
+    private Rigidbody2D _playerRigidbody;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int _canJump;
+    private int _canVeilJump;
+    [SerializeField]
+    private int _canJumpValue;
+    [SerializeField]
+    private int _canVeilJumpValue;
+    [SerializeField]
+    private float _veilJumpWidthScale;
+    private float _originalWidthScale;
+
+    private SpriteRenderer _spriteRenderer;
+    //private BoxCollider2D _BoxCollider2D;
+    //private CircleCollider2D _CircleCollider2D;
+
+    //Vector2 and float to store original Box Collider size and Circle collider radius respectively.
+    //private Vector2 _BoxColliderSize;
+    //private float _CircleColliderRadius;
+
+    //private Vector2 _spriteRendererSize;
+
+    //private Vector2 _BoxColliderOffset;
+    //private Vector2 _CircleColliderOffset;
+
+
+    private void Start()
+    {
+        _speed = _speedValue;
+        _canJump = _canJumpValue;
+        _canVeilJump = _canVeilJumpValue;
+        _originalWidthScale = transform.localScale.x;
+        //Debug.Log(_originalWidthScale);
+
+        //Fetch the Rigidbody component from the GameObject
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        //Fetch the SpriteRenderer component from the GameObject
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //Debug.Log(whatIsGround);
+
+        FindObjectOfType<AudioManager>().Play("MenuLoop");
+
+        ////gets box collider
+        //_BoxCollider2D = GetComponent<BoxCollider2D>();
+        ////stores boxcollider size and offset
+        //_BoxColliderSize = _BoxCollider2D.size;
+        //_BoxColliderOffset = _BoxCollider2D.offset;
+        //Debug.Log(_BoxCollider2D.size);
+        ////stores circlecollider radius and offset
+        //_CircleCollider2D = GetComponent<CircleCollider2D>();
+        //_CircleColliderRadius = _CircleCollider2D.radius;
+        //_CircleColliderOffset = _CircleCollider2D.offset;
+        //Debug.Log(_CircleCollider2D.radius);
+        ////stores _spriteRenderer size and offset
+        //_spriteRenderer = GetComponent<SpriteRenderer>();
+
+        ////_BoxCollider2D.size = new Vector2(0.05f, 0.3f);
+        //Debug.Log(_BoxColliderSize);
+        ////_CircleCollider2D.radius = 0.1f;
+        //Debug.Log(_CircleColliderRadius);
+
+        //UpdateColliderSize();
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        _playerRigidbody.velocity = new Vector2(horizontalMove * _speed, _playerRigidbody.velocity.y);
+
+        if (facingRight == false && horizontalMove > 0)
+        {
+            facingRight = !facingRight;
+            _spriteRenderer.flipX = false;
+        }
+        else if (facingRight == true && horizontalMove < 0)
+        {
+            facingRight = !facingRight;
+            _spriteRenderer.flipX = true;
+        }
+
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        horizontalMove = Input.GetAxis("Horizontal");
+
+        if (isGrounded == true)
+        {
+            _canJump = _canJumpValue;
+            _canVeilJump = _canVeilJumpValue;
+            _speed = _speedValue;
+        }
+
+        if (isGrounded == false && _playerRigidbody.velocity.y <= 0)
+        {
+            ResetVeilJump();
+        }
+
+        if (Input.GetButtonDown("Jump") && _canJump > 0)
+        {
+            Jump();
+        }
+
+        else if (Input.GetButtonDown("Veil Jump") && _canVeilJump > 0)
+        {
+            VeilJump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        //set coreect gravity and move player
+        ResetVeilJump();
+        _playerRigidbody.velocity = Vector2.up * jumpforce;
+        //play cirrect sound depending on single or double jump
+        if(_canJump == 2)
+        {
+            FindObjectOfType<AudioManager>().Play("PlayerJump");
+        }
+        else if (_canJump == 1)
+        {
+            FindObjectOfType<AudioManager>().Play("DoubleJump");
+        }
+        _canJump--;
+        _canVeilJump--;
+    }
+
+    private void VeilJump()
+    {
+        _playerRigidbody.gravityScale = 25;
+        //set x velocity to 0 and jump with veiljump property.
+        _playerRigidbody.velocity = new Vector2(0, 1 * _veilJumpForce);
+        FindObjectOfType<AudioManager>().Play("VeilJump");
+        _speed = 0;
+        transform.localScale = new Vector3(transform.localScale.x * _veilJumpWidthScale, transform.localScale.y, transform.localScale.z);
+        _canVeilJump--;
+        _canJump --;
+    }
+
+    private void ResetVeilJump()
+    {
+        _playerRigidbody.gravityScale = 3;
+        transform.localScale = new Vector3(_originalWidthScale, transform.localScale.y, transform.localScale.z);
+        _speed = _speedValue;
+        _playerRigidbody.velocity = new Vector2(horizontalMove * _speed, _playerRigidbody.velocity.y);
+    }
+
+
+    private void UpdateColliderSize()
+    {
+
+        //_BoxCollider2D.size = _spriteRenderer.sprite.bounds.size;
+        //_BoxCollider2D.offset = new Vector2(0, 0);
+        //_CircleCollider2D.radius = _spriteRenderer.sprite.bounds.size.x * 0.3317394f;
+
+        //Debug.Log(_spriteRenderer.sprite.bounds.size.x + " = sprite x value");
+    }
+
+}
