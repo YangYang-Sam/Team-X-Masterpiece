aa72ee88790f28616d793fb015863aef0fced5ad
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-10-09 16:35:29 +0100

    Add Enemy character and started simple patrol
    Initial logic.

---
+++ Programming/Game Prototype/Assets/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Plane 2"), LayerMask.NameToLayer("Plane 1"), true);
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Plane 2"), LayerMask.NameToLayer("Plane 2"), false);
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Plane 2"), LayerMask.NameToLayer("Plane 3"), true);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Patrol Marker")
+        {
+            moveSpeed *= -1;
+            Debug.Log("collisions working");
+            if (_spriteRenderer.flipX == true)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else
+            {
+                _spriteRenderer.flipX = true;
+            }
+        }
+        
+    }
+}
