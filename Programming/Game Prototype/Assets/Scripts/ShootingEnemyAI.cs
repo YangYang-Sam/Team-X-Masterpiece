785f996ddd0ac4c2e415ade52330b70741401338
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-11-01 17:58:42 +0000

    Implemented First New enemy behaviour
    Projectile firing enemy on plane 2. Unique script added for enemy and projectile.

---
+++ Programming/Game Prototype/Assets/Scripts/ShootingEnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemyAI : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _projectilePrefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(Shoot());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    private IEnumerator Shoot()
+    {
+        while (_projectilePrefab != null)
+        {
+            Instantiate(_projectilePrefab, transform.position + new Vector3(-0.5f, 0, 0), Quaternion.identity, transform);
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+
+}
