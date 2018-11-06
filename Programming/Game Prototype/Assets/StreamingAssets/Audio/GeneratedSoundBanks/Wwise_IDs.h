578fec8ed6a0ced5359c65db21e7447d7dff8e33
StuartH <stuarthosking@aol.co.uk>
2018-11-06 17:35:39 +0000

    A second wave of Wwise Menu Faff

---
+++ Programming/Game Prototype/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,113 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ALL_PLANE_MUSIC = 1985200777U;
+        static const AkUniqueID DOOR = 1877847629U;
+        static const AkUniqueID GHOST = 4023194814U;
+        static const AkUniqueID GHOUL = 3922529076U;
+        static const AkUniqueID JUMPING = 68157183U;
+        static const AkUniqueID MENULOOP = 3236081754U;
+        static const AkUniqueID PLATFORM = 4035573696U;
+        static const AkUniqueID PORTALS = 2017263062U;
+        static const AkUniqueID ROTATE = 1302771492U;
+        static const AkUniqueID SKELETON = 3478279848U;
+        static const AkUniqueID VEILJUMP = 1671131289U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MENU_OR_LEVEL
+        {
+            static const AkUniqueID GROUP = 1222409307U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LEVEL = 2782712965U;
+                static const AkUniqueID MENU = 2607556080U;
+            } // namespace STATE
+        } // namespace MENU_OR_LEVEL
+
+        namespace MUSIC_STATE
+        {
+            static const AkUniqueID GROUP = 3826569560U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AZTEC = 3866797368U;
+                static const AkUniqueID EASTERN = 3476384675U;
+                static const AkUniqueID EGYPTIAN = 3835820942U;
+                static const AkUniqueID MENUSTATE = 1548586727U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace DOOR
+        {
+            static const AkUniqueID GROUP = 1877847629U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace DOOR
+
+        namespace PLANE_SELECTION
+        {
+            static const AkUniqueID GROUP = 1818010914U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AZTEC = 3866797368U;
+                static const AkUniqueID EASTERN = 3476384675U;
+                static const AkUniqueID EGYPTIAN = 3835820942U;
+            } // namespace SWITCH
+        } // namespace PLANE_SELECTION
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAME_PAUSE = 2772308904U;
+        static const AkUniqueID MENU_MUSIC = 4055567060U;
+        static const AkUniqueID MUSIC_SLOWDOWN = 2779281842U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID PLATFORM = 4035573696U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TVEIL_LVL_SB = 928849380U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
