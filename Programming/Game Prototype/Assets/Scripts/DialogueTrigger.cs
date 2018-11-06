6f842796611b27a868509267a0d8f0c89b212ee0
Jonny-S <43474013+Jonny-S@users.noreply.github.com>
2018-11-06 21:18:38 +0000

    Added initial dialogue implementation

---
+++ Programming/Game Prototype/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
