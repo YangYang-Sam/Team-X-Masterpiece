200826502fcbd756807634a698187fe54fdc1901
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-12-03 17:10:39 +0000

    Added Volume Levels Script
    Also fixed options layout to accomodate new SFX sliders.

---
+++ Programming/Game Prototype/Assets/VolumeLevels.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeLevels : MonoBehaviour {
+
+    [SerializeField]
+    private Slider thisSlider;
+    private float musicVolume;
+    private float SFXVolume;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void setVolume(string whatVolume)
+    {
+        float sliderValue = thisSlider.value;
+
+        if(whatVolume == "Music")
+        {
+            musicVolume = thisSlider.value;
+            AkSoundEngine.SetRTPCValue("MusicVolume", musicVolume);
+        }
+
+        if (whatVolume == "SoundFX")
+        {
+            SFXVolume = thisSlider.value;
+            AkSoundEngine.SetRTPCValue("SFXVolume", SFXVolume);
+        }
+    }
+}
