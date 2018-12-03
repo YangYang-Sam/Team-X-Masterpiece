8d3e27e5e7b5d65303fff721b67979e2d03a1565
Jonny Shaw <43474013+Jonny-S@users.noreply.github.com>
2018-12-03 12:20:02 +0000

    Added Game Timer
    Time is displayed on victory screen.

--- Programming/Game Prototype/Assets/Scripts/GameManager.cs
+++ Programming/Game Prototype/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -10,19 +11,31 @@ public class GameManager : MonoBehaviour {
     public GameObject pauseMenuUI;
     public GameObject victoryScreenUI;
 
+    [SerializeField]
+    private Text timerText;
+    private float startTime;
+
     private bool timeAlreadyPaused = false;
 
     private bool victorious = false;
 
+
     // Use this for initialization
     void Start ()
     {
-
+        startTime = Time.time;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds + "!";
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (victorious == false)
